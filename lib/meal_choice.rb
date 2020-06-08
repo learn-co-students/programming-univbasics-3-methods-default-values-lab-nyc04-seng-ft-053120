ed90bebfac368fd9df4ff582c826fb7a8173c713
Patricia Dros <tricia.dros@gmail.com>
2020-06-08 20:27:27 +0000

    Done.

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -7,7 +7,7 @@ def meal_choice (veg1, veg2, protien = 'tofu')
   meal = "A plate of #{protien} with #{veg1} and #{veg2}."
   
   puts meal 
-  meal 
+
 end
 
 meal_choice('broccoli', 'carrots', 'lentils')
